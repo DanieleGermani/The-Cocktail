821480359e1d25f713f6484811e88f1948724fe7
Daniele <daniele.germani.85@gmail.com>
2017-10-07 13:58:47 +0200

    create section and footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componets/header/header.component';
+import { Section1Component } from './componets/section1/section1.component';
+import { Section2Component } from './componets/section2/section2.component';
+import { Section3Component } from './componets/section3/section3.component';
+import { FooterComponent } from './componets/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    Section1Component,
+    Section2Component,
+    Section3Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule
