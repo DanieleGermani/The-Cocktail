5265a31124ddc82c1a84ddda21ce8c771a124e34
Daniele <daniele.germani.85@gmail.com>
2017-10-07 15:55:42 +0200

    added section4 componets and html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { Section1Component } from './componets/section1/section1.component';
 import { Section2Component } from './componets/section2/section2.component';
 import { Section3Component } from './componets/section3/section3.component';
 import { FooterComponent } from './componets/footer/footer.component';
+import { Section4Component } from './componets/section4/section4.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { FooterComponent } from './componets/footer/footer.component';
     Section1Component,
     Section2Component,
     Section3Component,
-    FooterComponent
+    FooterComponent,
+    Section4Component
   ],
   imports: [
     BrowserModule
